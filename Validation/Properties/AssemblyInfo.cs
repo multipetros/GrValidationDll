b1e4b1c2fc63e3b9761b04ac7e7dd03617335d12
Petros Kyladitis <multipetros@users.noreply.github.com>
2013-03-13 01:08:10 +0200

    ViesGr valid range correction

--- Validation/Properties/AssemblyInfo.cs
+++ Validation/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Validation")]
-[assembly: AssemblyCopyright("Copyright 2012 Petros Kyladitis")]
+[assembly: AssemblyCopyright("Copyright 2012-2013, Petros Kyladitis")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
